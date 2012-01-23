4589bc38b751ba3c62709912aab40d4c97ed442a
Mathieu Lecarme <mathieu@garambrogne.net>
2012-01-23 21:07:19 +0100

    ruby code is now more orthodox.

--- spec/cluster_spec.rb
+++ spec/cluster_spec.rb
@@ -3,24 +3,25 @@ require "minitest/autorun"
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require "tempest"
 
-
 describe Tempest::Cluster do
 
   it "should connect and fetch some works to do" do
-    @@done = false
+    $msg = nil
+
     tempest do
       worker :working do
+
         on :foo do |context, name|
-          p "#{name} was here"
-          @@done = true
+          $msg = name
           context.stop
         end
 
         work :foo, nil, 'bar'
+
       end
     end
 
-    @@done.should be_true
+    $msg.should == 'bar'
 
   end
 end
