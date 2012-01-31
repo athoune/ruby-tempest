12c8fbc944c7a3331b3e71859e095d0826cf2fd2
Mathieu Lecarme <mathieu.lecarme@af83.com>
2012-01-31 11:40:44 +0100

    Count request.
    Signed-off-by: Mathieu Lecarme <mathieu.lecarme@af83.com>

--- examples/front.js
+++ examples/front.js
@@ -2,11 +2,19 @@ var redis = require('redis'),
     connect = require('connect'),
     tempest = require('tempest');
 
+var cpt = 0;
+
 var cluster = tempest.createCluster(function() {
     var web = connect.createServer(
         connect.favicon(),
         connect.router(function(app) {
+
+            app.get('/info', function(req, res, next) {
+                res.end("" + cpt);
+            });
+
             app.get('*', function(req, res, next) {
+                cpt += 1;
                 args = {
                     headers: req.headers,
                     method: req.method,
